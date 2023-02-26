3409e1bfa7fd7294b949f40b38d0848cd5e3eb4d
Hammadulhaq123 <professorcoding123@gmail.com>
2023-02-26 23:15:22 +0500

    updated little bit styling for proper theme swithching

--- src/components/LeftNavMenuItem.jsx
+++ src/components/LeftNavMenuItem.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const LeftNavMenuItem = ({text, icon, className, action}) => {
+const LeftNavMenuItem = ({text, icon, className, action, theme}) => {
   return (
-    <div className={`text-white text-sm cursor-pointer h-10 flex items-center px-3 mb-[1px] rounded-full hover:bg-[#046ec4]/[0.6] ${className}`} onClick={action}>
+    <div className={` text-sm cursor-pointer h-10 flex items-center px-3 mb-[1px]  rounded-full hover:bg-[#046ec4]/[0.6] hover:text-white ${className}`}  onClick={action} style={{color:theme==="dark"?"#fff":"#046ec4"}}>
       <span className='text-xl mr-5'>{icon}</span>
       {text}
     </div>
