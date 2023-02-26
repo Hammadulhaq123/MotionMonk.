0a48bd1cdec231bd8e0f3cb9361573034edf6644
Hammadulhaq123 <professorcoding123@gmail.com>
2023-02-26 23:17:16 +0500

    Added video card for each video that is being fetched from the API

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,11 +1,34 @@
-import React, { useState , useContext } from 'react'
+import React, { useState , useContext, useEffect } from 'react'
 import { Context } from "../context/contextApi";
 import LeftNav from "./LeftNav"
+import VideoCard from './VideoCard';
 
-const Feed = () => {
+const Feed = (props) => {
+  
+  
+  const { loading, searchResults } = useContext(Context);
+  
+  useEffect(()=>{
+    document.getElementById("root").classList.remove("custom-h");
+  }, [])
+
+
   return (
     <div className='flex flex-row h-[calc(100%-56px)]'>
-      <LeftNav />
+      <LeftNav theme={props.theme}/>
+      <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto " style={{background:props.theme==="dark"?"black":"white"}}>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5">
+          {!loading && searchResults &&
+          searchResults?.map( (item, i) => {
+            if (item?.type !== "video") return false;
+            return (
+              <VideoCard 
+              key={item.video.videoId} 
+              video={item.video} theme={props.theme}/>
+            )
+          })}
+        </div>
+      </div>
     </div>
   )
 }
